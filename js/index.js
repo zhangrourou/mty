289724300a072cd695e157197ff6f5231ebd39dc
zhangrong <932603484@qq.com>
2018-07-15 11:56:59 +0800

    提交文件

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*动画*/
+// $(function(){
+//     $(".ccp-cont-box1").mouseover(function(){
+//         $(this).addClass('animated flipOutY');
+//         setTimeout(function(){
+//             $('.ccp-cont-box1').removeClass('flipOutY');
+//         },1000);
+//     });
+//
+//
+//
+//
+// });
+
+
